3ebd5e0c452a9de70b73fa813b49df54720029d3
muhriddin-davlatov <muhriddind819@gmail.com>
2024-05-11 10:22:12 +0500

    change design

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
+import { getDownloadURL, listAll, ref, uploadBytes } from "firebase/storage";
 import React, { useEffect, useState } from "react";
 import { storage } from "./Firebase";
-import { getDownloadURL, listAll, ref, uploadBytes } from "firebase/storage";
+import Loader from "./components/Loader";
+import { v4 } from "uuid";
 
 function App() {
   const [image, setImage] = useState(null);
-  const [imageUrl, setImageUrl] = useState([]);
+  const [imageList, setImageList] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   const imageListRef = ref(storage, "images/");
 
-  const uploadFile = async () => {
-    if (image == null) return;
+  const imageUpload = (e) => {
+    e.preventDefault();
+    if (!image) {
+      return alert("Image Not Found");
+    }
+    setIsLoading(true);
 
-    const imageRef = ref(storage, `images/${image.name}`);
+    const imageRef = ref(storage, `images/${image.name + v4()}`);
 
     uploadBytes(imageRef, image).then((snapshot) => {
       getDownloadURL(snapshot.ref).then((url) => {
-        setImageUrl((prev) => [...prev]);
+        setImageList((prev) => [...prev, url]);
+        setIsLoading(false);
+        setImage("");
       });
     });
   };
@@ -24,30 +33,34 @@ function App() {
     listAll(imageListRef).then((response) => {
       response.items.forEach((item) => {
         getDownloadURL(item).then((url) => {
-          setImageUrl((prev) => [...prev, url]);
+          setImageList((prev) => [...prev, url]);
         });
       });
     });
   }, []);
 
   return (
-    <>
-      <div className="App">
-        <input
-          type="file"
-          onChange={(event) => {
-            setImage(event.target.files[0]);
-          }}
-        />
-        <button onClick={uploadFile}>Upload File</button>
-      </div>
+    <div>
+      <form className="upload__form">
+        <i
+          className="bx bx-cloud-upload"
+          style={image ? { color: "blue" } : { color: "gray" }}
+        ></i>
+        <input type="file" onChange={(e) => setImage(e.target.files[0])} />
+        <button onClick={imageUpload}>
+          {isLoading ? <Loader /> : "Upload Image"}
+        </button>
+      </form>
 
-      <div>
-        {imageUrl.map((url) => (
-          <img src={url} alt="" />
-        ))}
-      </div>
-    </>
+      <ul className="images__list">
+        {imageList &&
+          imageList.map((item) => (
+            <li key={item}>
+              <img src={item} alt="" />
+            </li>
+          ))}
+      </ul>
+    </div>
   );
 }
 
